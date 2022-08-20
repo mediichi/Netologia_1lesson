df167c584d0b380f7c828cfd40efc983e81384b8
cat101kris <>
2022-08-20 18:41:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Задачача №1
+
+let username = "my name";
+let bonusBalance = 1000;
+
+console.log(`Пользователь ${username}
+Баланс ${bonusBalance}`);
+
+//Задача №2
+
+let IncomeBalans = 50;
+let ExpensesBalans = 3;
+
+console.log(`Баланс ${bonusBalance + 3 * IncomeBalans - 7*3} пользователя через 7 дней`);
